c8bdf3a5f46b89522c43eb5957dedfd7731389e3
leadingmind <leadingmind@users.noreply.github.com>
2021-07-10 12:55:52 +0530

    initial commit

--- src/components/menu.js
+++ src/components/menu.js
@@ -2,17 +2,29 @@ import Card from "react-bootstrap/Card";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 
+const rowStyle = {
+  "overflow-x": "auto",
+  "white-space": "nowrap"
+};
+const rowItemStyle = {
+  display: "inline-block",
+  float: "none"
+};
 export default function Menu() {
   return (
     <Container>
-      <Row>
-        <Item />
+      <Row style={rowStyle}>
+        <Item style={rowItemStyle} />
+        <Item style={rowItemStyle} />
+        <Item style={rowItemStyle} />
+        <Item style={rowItemStyle} />
+        <Item style={rowItemStyle} />
       </Row>
     </Container>
   );
 }
 const Item = () => (
-  <Card style={{ width: "18rem" }}>
+  <Card style={{ width: "5rem" }}>
     <Card.Img
       variant="top"
       src="https://image.freepik.com/free-vector/hamburger_53876-25481.jpg"
