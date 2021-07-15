77cea9a79e45d9f372b84a735773e71cc29942f1
leadingmind <leadingmind@users.noreply.github.com>
2021-07-15 13:01:22 +0530

    Image transpiling problem not resolved

--- src/asset/data.js
+++ src/asset/data.js
@@ -1,26 +1,20 @@
 const items = [
-  { id: 1,
-    name: "Burger", 
-    image: "", 
-    thumbnail: "../asset/images/burger.jpg",
-    desc: "" },
+  { id: 1, name: "Burger", image: "", thumbnail: "/src/asset/images/burger.jpg", desc: "" },
   {
     id: 2,
     name: "Momo",
     image: "https://wanderwall.in/wp-content/uploads/2021/07/momo-1.png",
-    thumbnail: "../asset/images/momo.jpg",
+    thumbnail: "/src/asset/images/momo.jpg",
     desc: ""
   },
-  { id: 3, 
-    name: "Drumstick", 
-    image: "", 
-    thumbnail: "../asset/images/drumsticks.jpg",
-    desc: "" },
-  { id: 4, 
-    name: "Wing", 
-    image: "", 
-    thumbnail: "../asset/images/wings.jpg",
-    desc: "" }
+  {
+    id: 3,
+    name: "Drumstick",
+    image: "",
+    thumbnail: "/src/asset/images/drumsticks.jpg",
+    desc: ""
+  },
+  { id: 4, name: "Wing", image: "", thumbnail: "/src/asset/images/wings.jpg", desc: "" }
 ];
 
 export default items;
--- src/components/menuscroll.js
+++ src/components/menuscroll.js
@@ -4,12 +4,12 @@ import item from "../asset/data.js";
 import { Link } from "react-router-dom";
 
 const FoodCard = (props) => (
-  <div class="col-md-2 col-sm-3 col-xs-5">
+  <div className="col-md-2 col-sm-3 col-xs-5">
     <Card>
       <Card.Img
         style={{ width: props.width, margin: "auto" }}
         variant="top"
-        src={props.imgsrc}
+        src={require(props.imgsrc)}
       />
       <Card.Body>
         <Card.Title>{props.title}</Card.Title>
@@ -20,16 +20,15 @@ const FoodCard = (props) => (
 
 export default function Menuscroll() {
   return (
-    <div class="container-fluid">
-      <h3 class="subtitle">
+    <div className="container-fluid">
+      <h3 className="subtitle">
         Our improvised <span>Menus</span>
       </h3>
-      <div class="scrolling-wrapper row flex-row flex-nowrap mt-4 pb-4">
+      <div className="scrolling-wrapper row flex-row flex-nowrap mt-4 pb-4">
         {item.map((singleItem) => (
-          <Link to={`/item/${singleItem.name}`}>
+          <Link to={`/item/${singleItem.name}`} key={singleItem.id}>
             <FoodCard
-              key={singleItem.id}
-              imgsrc={require(singleItem.thumbnail)}
+              imgsrc={singleItem.thumbnail}
               title={singleItem.name}
               width="50px"
               height="auto"
